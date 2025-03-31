7b790e78b9027fbb826f068642659cbdd7fa331e
doug gilmore <d.gilmore@mac.com>
2025-03-31 00:50:40 -0500

    avante + noice + nvim-cmp + notify
    command line code completion; avante (cursor AI knock-off); notification
    plugins

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
-require 'core.options'  -- Load general options
-require 'core.keymaps'  -- Load general keymaps
+require 'core.options' -- Load general options
+require 'core.keymaps' -- Load general keymaps
 require 'core.snippets' -- Custom code snippets
 
 -- Install package manager
@@ -43,6 +43,7 @@ require('lazy').setup({
   require 'plugins.bufferline',
   require 'plugins.neo-tree',
   -- require 'plugins.alpha',
+  require 'plugins.noice',
   require 'plugins.beneliath',
   require 'plugins.indent-blankline',
   require 'plugins.lazygit',
@@ -55,6 +56,7 @@ require('lazy').setup({
   -- require 'plugins.chatgpt',
   require 'plugins.aerial',
   require 'plugins.vim-tmux-navigator',
+  require 'plugins.notify',
   require 'plugins.avante',
 }, {
   ui = {
